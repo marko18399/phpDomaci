a337683402ab05024646e3e9992820ac8949e5f1
marko18399 <marko.savicevic52@gmail.com>
2022-01-31 19:17:22 +0100

    osnovne funkcije u klasi Kontakt

--- includes/kontakti.php
+++ includes/kontakti.php
@@ -26,7 +26,34 @@
             $query = "SELECT * FROM stavke";
             return $conn->query($query);
         }
-        
+
+        public static function getById($id,mysqli $conn){
+            $query = "SELECT * FROM stavke WHERE id=$id";
+
+            $myObj=array();
+            if($msqlObj = $conn->query($query)){
+                while($red = $msqlObj->fetch_array(1)){
+                    $myObj[]=$red;
+                }
+            }
+            return $myObj;
+        }
+
+        public function deleteById(mysqli $conn){
+            $query="DELETE FROM stavke where id=$this->id";
+            return $conn->query($query);
+        }
+
+        public function update($id,mysqli $conn){
+            $query="UPDATE stavke SET ime = $this->ime, prezime=$this->prezime, telefon=$this->telefon, mesto=$this->mesto, rodjendan=$this->rodjendan, fakultet=$this->fakultet WHERE id=$id";
+            return $conn->query($query);
+        }
+
+        public static function add(Kontakt $kontakt,mysqli $conn){
+            $query = "INSERT INTO stavke(ime,prezime,telefon,mesto,rodjendan,fakultet) VALUES ('$kontakt->ime','$kontakt->prezime','$kontakt->telefon','$kontakt->mesto','$kontakt->rodjendan','$kontakt->fakultet')";
+            return $conn->query($query);
+        }
+
     }
 
 
